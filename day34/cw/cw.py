58446f5c5fe44698f4235c8ccf6338a08f518c39
aleqsandrewowkolauri <mikeyvaska@gmail.com>
2025-02-06 22:43:32 +0400

    commit

---
+++ day34/cw/cw.py
@@ -0,0 +1,18 @@
+list2 =[4,5,6,7,8,9,10,true, false, 3.5]
+
+list2.pop(1)
+
+
+
+def remove_eldments(main_list, indice list):
+    for ind in indices_list:
+        main_list.pop(ind)
+
+    print(main_list)
+
+
+
+    remove_eldments([1,2,3,4,5,6,7,8,9,10,][2,5,4,])
+    #[1,2,4,5,8,9,10]
+
+
---
+++ day34/hw/hw.py
@@ -0,0 +1,3 @@
+def add_numbers(num1, num2):
+    sum_result = num1 + num2
+    print("The sum is:", sum_result)
