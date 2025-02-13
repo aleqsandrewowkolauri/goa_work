50f73ff87efc3a2237b021f3fb53fd75aa1b58e2
aleqsandrewowkolauri <mikeyvaska@gmail.com>
2025-02-13 11:18:13 +0400

    commit

---
+++ day38/cw/cw.py
@@ -0,0 +1,4 @@
+tuple2 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+for index in range(len(tuple2)):
+    print(tuple2[index])
---
+++ day38/hw/hm.py
@@ -0,0 +1,10 @@
+my_set = {1, 2, 3, 4, 5}
+
+my_set.add(6)  # Adding 6 to the set
+
+my_set.remove(3)  # Removing 3 from the set
+
+is_present = 4 in my_set  # Checking if 4 is in the set
+
+print("Updated Set:", my_set)
+print("Is 4 present in the set?", is_present)
